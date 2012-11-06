f0452da8c2cc0e1e6274631def2570d283370aee
hypereddie10 <eddie@gamezgalaxy.com>
2012-11-05 21:51:34 -0500

    Added /mute #4

--- src/net/mcforge/irc/EventListener.java
+++ src/net/mcforge/irc/EventListener.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
-* Copyright (c) 2012 MCForge.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the GNU Public License v3.0
-* which accompanies this distribution, and is available at
-* http://www.gnu.org/licenses/gpl.html
-******************************************************************************/
+ * Copyright (c) 2012 MCForge.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ ******************************************************************************/
 package net.mcforge.irc;
 
 import net.mcforge.API.EventHandler;
@@ -15,11 +15,13 @@ import net.mcforge.API.server.ServerChatEvent;
 public class EventListener implements Listener {
 	@EventHandler
 	public void onPlayerChat(PlayerChatEvent e) {
-		IRCPlugin.getBot().handler.sendMessage(e.getPlayer().getName() + ": " + e.getMessage());
+		if (IRCPlugin.getBot() != null)
+			IRCPlugin.getBot().handler.sendMessage(e.getPlayer().getName() + ": " + e.getMessage());
 	}
 	@EventHandler
 	public void onServerChat(ServerChatEvent e) {
-		IRCPlugin.getBot().handler.sendMessage(e.getConsole().getName() + ": " + e.getMessage());
+		if (IRCPlugin.getBot() != null)
+			IRCPlugin.getBot().handler.sendMessage(e.getConsole().getName() + ": " + e.getMessage());
 	}
 }
 
---
+++ src/net/mcforge/plugin/commands/Mute.java
@@ -0,0 +1,97 @@
+package net.mcforge.plugin.commands;
+
+import java.util.ArrayList;
+
+import net.mcforge.API.CommandExecutor;
+import net.mcforge.API.EventHandler;
+import net.mcforge.API.Listener;
+import net.mcforge.API.player.PlayerChatEvent;
+import net.mcforge.API.plugin.Command;
+import net.mcforge.chat.ChatColor;
+import net.mcforge.iomodel.Player;
+import net.mcforge.server.Server;
+
+public class Mute extends Command implements Listener {
+
+	boolean init;
+	ArrayList<Player> muted = new ArrayList<Player>();
+	@Override
+	public void execute(CommandExecutor arg0, String[] arg1) {
+		init(arg0.getServer());
+		
+		if (arg1.length == 0) { help(arg0); return; }
+		
+		Player who = arg0.getServer().findPlayer(arg1[0]);
+		
+		if (who == null) {
+			arg0.sendMessage("The player entered does not exist!");
+			return;
+		}
+		if (who == arg0) {
+			if (muted.contains(arg0)) {
+				muted.remove(arg0);
+				arg0.sendMessage("You &bun-muted" + ChatColor.White + " yourself!");
+			}
+			else
+				arg0.sendMessage("You cannot mute yourself!");
+			return;
+		}
+		
+		if (muted.contains(who)) {
+			muted.remove(who);
+			who.getChat().serverBroadcast(who.getDisplayName() + ChatColor.White + " has been &un-muted");
+		}
+		else {
+			if (arg0 instanceof Player) {
+				Player p = (Player)arg0;
+				if (who != p && who.getGroup().permissionlevel >= p.getGroup().permissionlevel) {
+					p.sendMessage("Cannot mute someone of a higher or equal rank.");
+					return;
+				}
+			}
+			muted.add(who);
+			who.getChat().serverBroadcast(who.getDisplayName() + ChatColor.White + " has been &8muted");
+		}
+	}
+	
+	public void init(Server s) {
+		if (!init) {
+			s.getEventSystem().registerEvents(this);
+			init = true;
+		}
+	}
+
+	@Override
+	public int getDefaultPermissionLevel() {
+		return 100;
+	}
+
+	@Override
+	public String getName() {
+		return "mute";
+	}
+
+	@Override
+	public String[] getShortcuts() {
+		return new String[0];
+	}
+
+	@Override
+	public void help(CommandExecutor arg0) {
+		arg0.sendMessage("/mute <player> - Mutes or unmutes the player.");
+	}
+
+	@Override
+	public boolean isOpCommandDefault() {
+		return true;
+	}
+	
+	@EventHandler
+	public void onChat(PlayerChatEvent event) {
+		if (muted.contains(event.getPlayer())) {
+			event.getPlayer().sendMessage(ChatColor.Dark_Red + "You are muted!");
+			event.setCancel(true);
+		}
+	}
+
+}
