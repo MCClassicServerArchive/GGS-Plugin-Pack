542e5a57b35eced2309226a375bcc8dbb09c8c7f
Arrem <arrem.derplin@gmail.com>
2012-12-22 20:04:17 +0100

    Added some color to it +Added /color /tcolor and /title commands +Added /money command *Fixed summon not working if level is different *Weather -> Whether

---
+++ src/net/mcforge/plugin/commands/Money.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+* Copyright (c) 2012 MCForge.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the GNU Public License v3.0
+* which accompanies this distribution, and is available at
+* http://www.gnu.org/licenses/gpl.html
+******************************************************************************/
+package net.mcforge.plugin.commands;
+
+import net.mcforge.API.CommandExecutor;
+import net.mcforge.API.ManualLoad;
+import net.mcforge.API.plugin.Command;
+import net.mcforge.iomodel.Player;
+
+@ManualLoad
+public class Money extends Command  {
+	@Override
+	public String[] getShortcuts() {
+		return new String[0];
+	}
+
+	@Override
+	public String getName() {
+		return "money";
+	}
+
+	@Override
+	public boolean isOpCommandDefault() {
+		return false;
+	}
+
+	@Override
+	public int getDefaultPermissionLevel() {
+		return 0;
+	}
+
+	@Override
+	public void execute(CommandExecutor executor, String[] args) {
+		Player who;
+		if (args.length > 0) {
+			who = executor.getServer().findPlayer(args[0]);
+			if (who == null) {
+				executor.sendMessage("Player not found!");
+				return;
+			}
+		}
+		else {
+			if (executor instanceof Player) {
+				who = (Player)executor;
+			}
+			else {
+				executor.sendMessage("Please specify a player!");
+				help(executor);
+				return;
+			}
+		}
+		
+		executor.sendMessage(who.getName() + " has &6" + who.getValue("mcf_money") + " " + executor.getServer().CurrencyName);
+	}
+
+	@Override
+	public void help(CommandExecutor executor) {
+		executor.sendMessage("/money <player> - shows the player's money");
+		if (executor instanceof Player)
+			executor.sendMessage("/money - shows your money");
+	}
+}
